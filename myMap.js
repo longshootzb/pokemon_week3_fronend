5b8375f5f3f9e7fceaa7b149f26fd431a992fd37
chenditc-bittiger <chenditc.bittiger@gmail.com>
2016-10-15 22:29:05 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -3,6 +3,7 @@ function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AikCEUj_rGAWu8x2XBM0lIgs25BcQN7AVFa7vTvQbpsVSMbcdTZB_7XgZJlR0t6g'
     });
+    add_pokemon_layer();
 }
 
 // 1. Define pokemon data format, create mock pokemon data
@@ -22,8 +23,11 @@ function get_pokemon_layer_from_map_items(map_items) {
     layer.add(pushpins);
     return layer;
 }
-var pokemon_layer = get_pokemon_layer_from_map_items(map_items)
-map.layers.insert(layer);
+function add_pokemon_layer() {
+    var pokemon_layer = get_pokemon_layer_from_map_items(map_items)
+    map.layers.insert(layer);
+}
+
 
 // 3. Add pokemon counter down refresh.
 
